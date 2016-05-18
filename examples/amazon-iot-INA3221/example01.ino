f7b41e79e3cc0c5ca62dff64e848301ce1bb2039
bmcdonough <wilmcd@gmail.com>
2016-05-18 15:53:40 -0400

    finally finished, need to add more than 1 value

--- examples/amazon-iot-INA3221/example01.ino
+++ examples/amazon-iot-INA3221/example01.ino
@@ -23,23 +23,28 @@
 #include <AmazonIOTClient.h>
 #include <ESP8266AWSImplementations.h>
 #include <AWSFoundationalTypes.h>
+#include <Wire.h>
+#include <SDL_Arduino_INA3221.h>
+SDL_Arduino_INA3221 ina3221;
 //WILMCD: add a keys.cpp and add values for everything that's in keys.h
 #include "keys.h"
 
-//uint8_t sensorEnable = D8;
-uint8_t sensor = A0;
-//uint8_t sensorSwitch = D1;
 
-const int sleepTimeS = 30;
+const int sleepTimeS = 60;
 
 void printWiFiData();
 void printCurrentNetwork();
 bool switchIsDisabled();
 void publish(const char *topic, uint16_t data);
-void checkAnalogVolt();
+void checkDigitalVoltage();
+
+// the three channels of the INA3221 named for SunAirPlus Solar Power Controller channels (www.switchdoc.com)
+#define LIPO_BATTERY_CHANNEL 1
+#define SOLAR_CELL_CHANNEL 2
+#define OUTPUT_CHANNEL 3
+
 
 void setup() {
-
   Serial.begin(9600);
 
   // Pin configuration
@@ -48,16 +53,20 @@ void setup() {
 
   Serial.println("Started!");
 
-  checkAnalogVolt();
+  Serial.println("Measuring voltage and current with ina3221 ...");
+  ina3221.begin();
+
+  checkDigitalVoltage();
   Serial.println("SleepyTime");
   ESP.deepSleep(sleepTimeS * 1000000);
 }
 
+
 void loop() {
 }
 
+
 void printWiFiData() {
-  
   // IP address
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
@@ -79,8 +88,8 @@ void printWiFiData() {
   Serial.println(mac[0], HEX);
 }
 
+
 void printCurrentNetwork() {
-  
   // SSID
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
@@ -90,10 +99,12 @@ void printCurrentNetwork() {
   Serial.println(WiFi.RSSI());
 }
 
+
 bool sensorIsDisabled() {
 
 //  return digitalRead(sensorSwitch) == LOW;
 }
+
 
 void publish(const char *topic, float data) {
   
@@ -127,7 +138,7 @@ void publish(const char *topic, float data) {
   iotClient.setAWSRegion(awsIotRegion);
   iotClient.setAWSEndpoint(awsIotEndpoint);
   iotClient.setAWSDomain(awsIotDomain);
-  iotClient.setAWSPath("/things/analog-volt/shadow");
+  iotClient.setAWSPath("/things/digital-voltage/shadow");
   iotClient.setAWSKeyID(awsKeyID);
   iotClient.setAWSSecretKey(awsSecKey);
   iotClient.setHttpClient(&httpClient);
@@ -137,7 +148,7 @@ void publish(const char *topic, float data) {
 
   Serial.println("Updating thing shadow...");
   
-  MinimalString shadow = ("{\"state\":{\"reported\":{\"voltage\":" + String(data, DEC) + "}}}").c_str();
+  MinimalString shadow = ("{\"state\":{\"reported\":{\"" + String(topic) + "\":" + String(data, 2) + "}}}").c_str();
   char* result = iotClient.update_shadow(shadow, actionError);
   delay(50);
 
@@ -152,35 +163,58 @@ void publish(const char *topic, float data) {
   Serial.println(result);
 }
 
-void checkAnalogVolt() {
 
-  Serial.println("Checking soil moisture level");
+void checkDigitalVoltage() {
+  Serial.println("------------------------------");
+  float shuntvoltage1 = 0;
+  float busvoltage1 = 0;
+  float current_mA1 = 0;
+  float loadvoltage1 = 0;
 
-  if (sensorIsDisabled()) {
-    Serial.println("Soil check is disabled..");
-    return;
-  }
 
-//  digitalWrite(sensorEnable, HIGH);
-  delay(100);
+  busvoltage1 = ina3221.getBusVoltage_V(LIPO_BATTERY_CHANNEL);
+  shuntvoltage1 = ina3221.getShuntVoltage_mV(LIPO_BATTERY_CHANNEL);
+  current_mA1 = -ina3221.getCurrent_mA(LIPO_BATTERY_CHANNEL);  // minus is to get the "sense" right.   - means the battery is charging, + that it is discharging
+  loadvoltage1 = busvoltage1 + (shuntvoltage1 / 1000);
+  
+  Serial.print("LIPO_Battery Bus Voltage:   "); Serial.print(busvoltage1); Serial.println(" V");
+  Serial.print("LIPO_Battery Shunt Voltage: "); Serial.print(shuntvoltage1); Serial.println(" mV");
+  Serial.print("LIPO_Battery Load Voltage:  "); Serial.print(loadvoltage1); Serial.println(" V");
+  Serial.print("LIPO_Battery Current 1:       "); Serial.print(current_mA1); Serial.println(" mA");
+  Serial.println("");
 
-  int sensorValue = analogRead(sensor);
-  float voltage = sensorValue * (3.3 / 539);
-//voltage = round(voltage*10)/10;
+  float shuntvoltage2 = 0;
+  float busvoltage2 = 0;
+  float current_mA2 = 0;
+  float loadvoltage2 = 0;
 
-//  digitalWrite(sensorEnable, LOW);
+  busvoltage2 = ina3221.getBusVoltage_V(SOLAR_CELL_CHANNEL);
+  shuntvoltage2 = ina3221.getShuntVoltage_mV(SOLAR_CELL_CHANNEL);
+  current_mA2 = -ina3221.getCurrent_mA(SOLAR_CELL_CHANNEL);
+  loadvoltage2 = busvoltage2 + (shuntvoltage2 / 1000);
+  
+  Serial.print("Solar Cell Bus Voltage 2:   "); Serial.print(busvoltage2); Serial.println(" V");
+  Serial.print("Solar Cell Shunt Voltage 2: "); Serial.print(shuntvoltage2); Serial.println(" mV");
+  Serial.print("Solar Cell Load Voltage 2:  "); Serial.print(loadvoltage2); Serial.println(" V");
+  Serial.print("Solar Cell Current 2:       "); Serial.print(current_mA2); Serial.println(" mA");
+  Serial.println("");
 
-  Serial.print("Sensor value: ");
-  Serial.println(sensorValue);
+  float shuntvoltage3 = 0;
+  float busvoltage3 = 0;
+  float current_mA3 = 0;
+  float loadvoltage3 = 0;
 
-//  uint8_t moistureLevel = sensorValue / 1023.0f * 100;
-//uint8_t moistureLevel = 233;
-  Serial.print("voltage: ");
-  Serial.println(voltage);
-   
-//  Serial.print("Moisture level: ");
-//  Serial.println(moistureLevel);
- 
-  publish("soil/moisture", voltage);
+  busvoltage3 = ina3221.getBusVoltage_V(OUTPUT_CHANNEL);
+  shuntvoltage3 = ina3221.getShuntVoltage_mV(OUTPUT_CHANNEL);
+  current_mA3 = ina3221.getCurrent_mA(OUTPUT_CHANNEL);
+  loadvoltage3 = busvoltage3 + (shuntvoltage3 / 1000);
+  
+  Serial.print("Output Bus Voltage 3:   "); Serial.print(busvoltage3); Serial.println(" V");
+  Serial.print("Output Shunt Voltage 3: "); Serial.print(shuntvoltage3); Serial.println(" mV");
+  Serial.print("Output Load Voltage 3:  "); Serial.print(loadvoltage3); Serial.println(" V");
+  Serial.print("Output Current 3:       "); Serial.print(current_mA3); Serial.println(" mA");
+  Serial.println("");
+
+  publish("busvoltage1", busvoltage1);
 }
 
