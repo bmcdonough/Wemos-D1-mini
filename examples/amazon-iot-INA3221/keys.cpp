91d8c752ecd31a8239349eb33dcf7cfc0460b704
bmcdonough <wilmcd@gmail.com>
2016-05-18 13:43:08 -0400

    need wifi support includes

---
+++ examples/amazon-iot-INA3221/keys.cpp
@@ -0,0 +1,19 @@
+// keys.cpp
+#include "keys.h"
+
+// aws
+const char* awsKeyID = "Your AWS Key Here";
+const char* awsSecKey = "Your AWS Secret Here";
+
+// aws iot
+// example:  https://ABCDEFGHIJKLMNO.iot.us-east-1.amazonaws.com/things/Thing-101/shadow
+const char* awsIotRegion = "us-east-1";
+const char* awsIotEndpoint = "amazon.com";
+const char* awsIotDomain = "ABCDEFGHIJKLMNO.iot.us-east-1.amazonaws.com";
+
+// wifi
+const char* wifiSsid = "Guest";
+const char* wifiPwd = "guest";
+
+// dateTimeProvider
+const char* dateTimeProvider = "1.2.3.4";
